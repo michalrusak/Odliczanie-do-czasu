d957be20d84d3a4c285345252fd4c93d4e7dc225
michalrusak <michal1406@onet.pl>
2022-05-12 18:57:22 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('.input'); 
+const form = document.querySelector('form');
+
+
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+const spanS = document.querySelector('span.s');
+
+const czas = (e) => {
+    e.preventDefault();
+    const word = input.value;
+    console.log(word);
+    if(input.value == "") return;
+
+    const endTime = new Date(word).getTime();
+    
+    setInterval(() => {
+        const nowTime = new Date().getTime();
+
+        const time = endTime - nowTime;
+        const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+       
+        let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+         
+        hours = hours < 10 ? `0${hours}` : hours;
+       
+        const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+       
+        const secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+       
+        spanD.textContent = days;
+        spanH.textContent = hours;
+        spanM.textContent = minutes;
+        spanS.textContent = secs;
+       }, 1000)
+
+    input.value= "";
+}
+
+form.addEventListener('submit', czas);
